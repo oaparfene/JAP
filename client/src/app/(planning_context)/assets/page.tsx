eca96070645a51bc60ed2b1ceacae7a6c01abd1b
Ovidiu Parfene <oaparfene@gmail.com>
2024-02-15 13:31:44 +0100

    multipleSelect add on both res and assets

--- client/src/app/(planning_context)/assets/page.tsx
+++ client/src/app/(planning_context)/assets/page.tsx
@@ -4,7 +4,7 @@ import { PlanSelector } from "@/components/PlanSelector"
 import { generateDataFromORBAT } from "@/constants"
 import { useContext, useEffect } from "react"
 import { JAPContext } from "../../context"
-import { Alert, Box, Button, Chip, IconButton, MenuItem, OutlinedInput, Select, Snackbar, Tab, Tabs, Typography } from "@mui/material"
+import { Alert, Box, Button, Chip, IconButton, MenuItem, Modal, OutlinedInput, Select, Snackbar, Stack, Tab, Tabs, Typography } from "@mui/material"
 import { DataGrid, GridColDef, GridRowId, GridSelectionModel } from "@mui/x-data-grid"
 import { useState } from "react"
 import MapView from "@/components/MapView"
@@ -22,7 +22,17 @@ const ClientSideMapView = dynamic(() => import('../../../components/MapView'), {
     ssr: false,
 });
 
-
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 interface TabPanelProps {
     children?: React.ReactNode;
@@ -65,15 +75,17 @@ export default function Home() {
     const [open, setOpen] = useState(false);
     const { fetchAssetsFromBackend, uploadAssetToBackend } = useData();
     const [rows, setRows] = useState<Asset[]>([])
-
+    const [selectedPlanName, setSelectedPlanName] = useState(allPlans[activePlanIndex]?.name || '')
+    const [openSelectPlan, setOpenSelectPlan] = useState(false)
+    
     useEffect(() => {
         setRows(allAssets)
     }, [allAssets])
 
-    const handlePlanSelectionChange = (rowId: any, event: any) => {
+    const handlePlanSelectionChange = (rowId: any, _planID: any) => {
         setRows((prevRows: any) =>
             prevRows.map((row: any) =>
-                row.ID === rowId ? { ...row, Plans_containing_self: event?.target.value } : row
+                row.ID === rowId ? { ...row, Plans_containing_self:_planID } : row
             )
         );
     };
@@ -85,7 +97,7 @@ export default function Home() {
                 multiple
                 value={value}
                 onChange={(e, node) => {
-                    handlePlanSelectionChange(param.id, e)
+                    handlePlanSelectionChange(param.id, e?.target.value)
                 }}
                 input={<OutlinedInput id="select-multiple-chip" sx={{ w: '200px' }} />}
                 renderValue={(selected) => (
@@ -211,6 +223,17 @@ export default function Home() {
         setSelectedRows([])
     }
 
+    const addMultipleSelectionToPlanHandler = () => {
+        if (selectedRows.length === 0) {
+            return
+        }
+        setOpenSelectPlan(true)
+        // console.log('plans', allPlans)
+        // setAmountOfAssetsAdded(selectedRows.length)
+        // setOpen(true);
+        // setSelectedRows([])
+    }
+
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
         if (reason === 'clickaway') {
             return;
@@ -254,9 +277,11 @@ export default function Home() {
                     </IconButton>
 
                 </Box>
+                <Box sx={{ display: 'flex', justifyContent: "start", width: 'full' }}>
 
-                <Button variant='contained' sx={{ mb: 2 }} onClick={saveAssetsHandler}>Save</Button>
-
+                <Button variant='contained' sx={{ mb: 2, mr: 2 }} onClick={saveAssetsHandler}>Save</Button>
+                <Button variant='contained' sx={{ mb: 2, ml: 2 }} onClick={addMultipleSelectionToPlanHandler}>Add to</Button>
+                    </Box>
                 {/* <Button variant='contained' sx={{ mb: 2 }} onClick={addToPlanHandler}>Add Selection to Plan</Button> */}
 
                 <Box sx={{ height: 650, width: '100%' }}>
@@ -289,6 +314,67 @@ export default function Home() {
                     Added {amountOfAssetsAdded} Assets to Plan {allPlans[activePlanIndex]?.name}
                 </Alert>
             </Snackbar>
+
+            <Modal
+                open={openSelectPlan}
+                onClose={() => setOpenSelectPlan(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Stack gap={2}>
+
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Choose which Plan to add selection to:
+                        </Typography>
+
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={selectedPlanName}
+                            label="Plan"
+                            onChange={(e) => {
+                                setSelectedPlanName(e.target.value)
+                                //selectedRows.map((rowId) => handlePlanSelectionChange(rowId, allPlans.find(plan => plan.name === e.target.value)!.db_id))
+                                //setOpenSelectPlan(false)
+                                //setOpen(true)
+                            }}
+                        >
+                            {allPlans.map((plan, index) => (
+                                <MenuItem key={index} value={plan?.name}>{plan?.name}</MenuItem>
+                            ))}
+                                <MenuItem value={"[REMOVE FROM ALL PLANS]"}>{"[REMOVE FROM ALL PLANS]"}</MenuItem>
+                        </Select>
+                        <Box sx={{ display: "flex", justifyContent: "space-between", width: "100%", gap: "8px"}}>
+
+                            <Button variant='outlined' onClick={
+                                () => {
+                                    setOpenSelectPlan(false)
+                                }
+                            }>
+                                Cancel
+                            </Button>
+                            <Button variant='contained'
+                            onClick={() => {
+                                if (selectedPlanName === "[REMOVE FROM ALL PLANS]") {
+                                    selectedRows.map((rowId) => handlePlanSelectionChange(rowId, ""))
+
+                                } else {
+                                    selectedRows.map((rowId) => handlePlanSelectionChange(rowId, allPlans.find(plan => plan.name === selectedPlanName)!.db_id))
+                                }
+                                setOpenSelectPlan(false)
+                                setSelectedRows([])
+                                //setOpen(true)
+                            }}>
+                                Confirm
+                            </Button>
+                        </Box>
+
+                    </Stack>
+
+
+                </Box>
+            </Modal>
         </Box>
     )
 }
--- client/src/app/(planning_context)/requirements/page.tsx
+++ client/src/app/(planning_context)/requirements/page.tsx
@@ -3,7 +3,7 @@
 import { PlanSelector } from "@/components/PlanSelector"
 import { useContext, useEffect } from "react"
 import { JAPContext } from "../../context"
-import { Alert, Box, Button, Chip, IconButton, MenuItem, OutlinedInput, Select, Snackbar, Tab, Tabs, Typography } from "@mui/material"
+import { Alert, Box, Button, Chip, IconButton, MenuItem, Modal, OutlinedInput, Select, Snackbar, Stack, Tab, Tabs, Typography } from "@mui/material"
 import { DataGrid, GridColDef, GridRowId, GridSelectionModel } from "@mui/x-data-grid"
 import { useState } from "react"
 import MapView from "@/components/MapView"
@@ -22,7 +22,17 @@ const ClientSideMapView = dynamic(() => import('../../../components/MapView'), {
     ssr: false,
 });
 
-
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
 
 interface TabPanelProps {
     children?: React.ReactNode;
@@ -65,6 +75,8 @@ export default function Home() {
     const [open, setOpen] = useState(false);
     const { fetchCRsFromBackend, uploadCRtoBackend } = useData();
     const [rows, setRows] = useState<Requirement[]>([])
+    const [openSelectPlan, setOpenSelectPlan] = useState(false)
+    const [selectedPlanName, setSelectedPlanName] = useState(allPlans[activePlanIndex]?.name || '')
 
     useEffect(() => {
         setRows(allRequirements)
@@ -74,10 +86,10 @@ export default function Home() {
 
     //const rows = allRequirements.filter((cr) => !allPlans[activePlanIndex]?.requirements?.find(req => req.ID === cr.ID))
 
-    const handlePlanSelectionChange = (rowId: any, event: any) => {
+    const handlePlanSelectionChange = (rowId: any, _planID: any) => {
         setRows((prevRows: any) =>
             prevRows.map((row: any) =>
-                row.ID === rowId ? { ...row, Plans_containing_self: event?.target.value } : row
+                row.ID === rowId ? { ...row, Plans_containing_self: _planID } : row
             )
         );
     };
@@ -89,7 +101,7 @@ export default function Home() {
                 multiple
                 value={value}
                 onChange={(e, node) => {
-                    handlePlanSelectionChange(param.id, e)
+                    handlePlanSelectionChange(param.id, e?.target.value)
                 }}
                 input={<OutlinedInput id="select-multiple-chip" sx={{ w: '200px' }} />}
                 renderValue={(selected) => (
@@ -322,7 +334,7 @@ export default function Home() {
             // loop over all requirements that have been edited
             if (!allRequirements.includes(row)) {
                 console.log('row changed: ', row)
-                
+
                 // loop over all plans that contain the edited requirement
                 row.Plans_containing_self?.map((planId) => {
                     // if the plan already contains the requirement, do nothing
@@ -342,7 +354,7 @@ export default function Home() {
                         //removeCRsFromPlan([row], plan.db_id)
                     }
                 })
-                
+
                 uploadCRtoBackend([row])
             }
 
@@ -358,6 +370,17 @@ export default function Home() {
         setAmountOfAssetsAdded(selectedRows.length)
         setOpen(true);
         setSelectedRows([])
+    }
+
+    const addMultipleSelectionToPlanHandler = () => {
+        if (selectedRows.length === 0) {
+            return
+        }
+        setOpenSelectPlan(true)
+        // console.log('plans', allPlans)
+        // setAmountOfAssetsAdded(selectedRows.length)
+        // setOpen(true);
+        // setSelectedRows([])
     }
 
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
@@ -406,7 +429,11 @@ export default function Home() {
 
                 <Box sx={{ display: 'flex', justifyContent: "space-between", width: 'full' }}>
                     {/* <Button variant='contained' sx={{ mb: 2 }} onClick={addToPlanHandler}>Add Selection to Plan</Button> */}
-                    <Button variant='contained' sx={{ mb: 2 }} onClick={saveRequirementsHandler}>Save</Button>
+                    <Box sx={{ display: 'flex', justifyContent: "start", width: 'full' }}>
+
+                        <Button variant='contained' sx={{ mb: 2, mr: 2 }} onClick={saveRequirementsHandler}>Save</Button>
+                        <Button variant='contained' sx={{ mb: 2, ml: 2 }} onClick={addMultipleSelectionToPlanHandler}>Add to</Button>
+                    </Box>
                     <EXCELReqUpload />
                 </Box>
 
@@ -440,6 +467,67 @@ export default function Home() {
                     Your changes have been Saved!
                 </Alert>
             </Snackbar>
+
+            <Modal
+                open={openSelectPlan}
+                onClose={() => setOpenSelectPlan(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Stack gap={2}>
+
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            Choose which Plan to add selection to:
+                        </Typography>
+
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={selectedPlanName}
+                            label="Plan"
+                            onChange={(e) => {
+                                setSelectedPlanName(e.target.value)
+                                //selectedRows.map((rowId) => handlePlanSelectionChange(rowId, allPlans.find(plan => plan.name === e.target.value)!.db_id))
+                                //setOpenSelectPlan(false)
+                                //setOpen(true)
+                            }}
+                        >
+                            {allPlans.map((plan, index) => (
+                                <MenuItem key={index} value={plan?.name}>{plan?.name}</MenuItem>
+                            ))}
+                                <MenuItem value={"[REMOVE FROM ALL PLANS]"}>{"[REMOVE FROM ALL PLANS]"}</MenuItem>
+                        </Select>
+                        <Box sx={{ display: "flex", justifyContent: "space-between", width: "100%", gap: "8px"}}>
+
+                            <Button variant='outlined' onClick={
+                                () => {
+                                    setOpenSelectPlan(false)
+                                }
+                            }>
+                                Cancel
+                            </Button>
+                            <Button variant='contained'
+                            onClick={() => {
+                                if (selectedPlanName === "[REMOVE FROM ALL PLANS]") {
+                                    selectedRows.map((rowId) => handlePlanSelectionChange(rowId, ""))
+
+                                } else {
+                                    selectedRows.map((rowId) => handlePlanSelectionChange(rowId, allPlans.find(plan => plan.name === selectedPlanName)!.db_id))
+                                }
+                                setOpenSelectPlan(false)
+                                setSelectedRows([])
+                                //setOpen(true)
+                            }}>
+                                Confirm
+                            </Button>
+                        </Box>
+
+                    </Stack>
+
+
+                </Box>
+            </Modal>
         </Box>
     )
 }
